d117d6e41e6b288611939c291d788434710ff12b
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-05-26 17:47:11 -0400

    created abstract class Dto in order to implement generic DAO which requires the ability to call getId() on an object. Since all objects have an id of type Long, I will have all DTO classes extend this abstract Dto.

---
+++ src/main/java/yogaLMS/dto/Dto.java
@@ -0,0 +1,14 @@
+package yogaLMS.dto;
+
+public abstract class Dto {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
