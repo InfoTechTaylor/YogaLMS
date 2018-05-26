e667addd621ae7d4e0ecb4e54d5b3e47070f73a6
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-05-26 17:48:42 -0400

    GenericDao interface for defining CRUD methods that implementation must adhere to.

---
+++ src/main/java/yogaLMS/dao/GenericDao.java
@@ -0,0 +1,39 @@
+package yogaLMS.dao;
+
+import yogaLMS.dto.Dto;
+
+/**
+ * This interface is setup for the generic CRUD methods
+ * for DAO classes. T will be the type of object that
+ * will be retrieved from storage or written to storage.
+ */
+public interface GenericDao<T extends Dto> {
+    /**
+     * Persist the newEntity object into storage
+     * @param newEntity of Type T
+     * @return newEntity of Type T
+     */
+    T create(T newEntity);
+
+
+    /**
+     * Retrieve an object/entity from storage using the indicated id as primary key
+     * @param id of object to be read
+     * @return object requested from file
+     */
+    T read(Long id);
+
+
+    /**
+     * Save changes made to a persistent object/entity
+     * @param updatedEntity of type T
+     */
+    void update(T updatedEntity);
+
+
+    /**
+     * Delete persisted entity
+     * @param persistentEntity of type T
+     */
+    void delete(T persistentEntity);
+}
