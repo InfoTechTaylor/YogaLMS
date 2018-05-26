3bb9d4bf484a81f4b1790308cc970259f7746145
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-05-26 17:53:59 -0400

    Generic class implementation of GenericDao, setup for in memory implementation of any object that extends the Dto abstract class. Can improve on this later by making it work for file implementations.

---
+++ src/main/java/yogaLMS/dao/GenericDaoImpl.java
@@ -0,0 +1,39 @@
+package yogaLMS.dao;
+
+import yogaLMS.dto.Dto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GenericDaoImpl<T extends Dto> implements GenericDao<T> {
+
+    private Map<Long, T> map = new HashMap<>();
+    private Long currentHighestId = 0L;
+
+    @Override
+    public T create(T newEntity) {
+        newEntity.setId(getNextId());
+        this.map.put(newEntity.getId(), newEntity);
+        return newEntity;
+    }
+
+    private Long getNextId(){
+        currentHighestId++;
+        return currentHighestId;
+    }
+
+    @Override
+    public void update(T updatedEntity) {
+        map.replace(updatedEntity.getId(), updatedEntity);
+    }
+
+    @Override
+    public void delete(T persistentEntity) {
+        map.remove(persistentEntity.getId());
+    }
+
+    @Override
+    public T read(Long id) {
+        return map.get(id);
+    }
+}
